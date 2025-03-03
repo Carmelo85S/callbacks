bcbc0d59223086d65da9fc32d34cd6bcaafa4d1a
Carmelo85S <carmelo.salis85@gmail.com>
2025-03-03 09:31:05 +0100

    callbacks

---
+++ aboutMe.js
@@ -0,0 +1,42 @@
+const fetchData = (printName, printAge, printLocation, printHobby) => {
+    console.log("Fetching data...")
+
+    setTimeout (() => {
+        const nameData = "My name is Carmelo";
+        printName(nameData);
+    }, 2000);
+
+    setTimeout (() => {
+        const ageData = "I am 40 years old";
+        printAge(ageData);
+    }, 3000);
+
+    setTimeout (() => {
+        const locationData = "I was born in Sardinia but i live in Sweden"
+        printLocation(locationData);
+    }, 4000)
+
+    setTimeout (() => {
+        const hobbyData = "My hobbies are do activities with my family, dance with my kids, photo and coding"
+        printHobby(hobbyData);
+    }, 5000)
+}
+
+
+const printName = (nameData) => {
+    console.log(nameData)
+}
+
+const printAge = (ageData) => {
+    console.log(ageData);
+}
+
+const printLocation = (locationData) => {
+    console.log(locationData)
+}
+
+const printHobby = (hobbyData) => {
+    console.log(hobbyData);
+}
+
+fetchData(printName, printAge, printLocation, printHobby);
